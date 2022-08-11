431afc60961c27df4fb47ffaf92d8aafc63afd7f
Michael Blin <mblin@figarocms.fr>
2022-08-11 17:31:19 +0200

    update MB_Button

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
 		.package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.0.1"),
-		.package(url: "https://github.com/Bejil/MB_Button", from: "1.0.0"),
+		.package(url: "https://github.com/Bejil/MB_Button", from: "1.0.21"),
 		.package(url: "https://github.com/Bejil/MB_TextField", from: "1.0.0")
     ],
     targets: [
